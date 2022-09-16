cf64ecb7779b30220b6914bdbbc3189d52299539
Armadindon <baptisteperrin77@gmail.com>
2022-09-16 17:31:02 +0300

    Ajout modele de base

---
+++ src/Model/types.d.ts
@@ -0,0 +1,67 @@
+/** Upload representation in strapi api */
+export type Upload = {
+  id?: number;
+  name: string;
+  alternativeText?: string;
+  caption?: string;
+  width: number;
+  height: number;
+  formats?: {
+    thumbnail?: Upload;
+    small?: Upload;
+    medium?: Upload;
+    large?: Upload;
+  };
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  createdAt?: string;
+  updatedAt?: string;
+};
+
+/** Type représentant un utilisateur dans le backend */
+export type User = {
+  id: number;
+  username: string;
+  email: string;
+  provider: string;
+  confirmed: boolean;
+  blocked: boolean;
+  createdAt: string;
+  updatedAt: string;
+};
+
+/** Type représentant un produit dans le backend */
+export type Product = {
+  id: 1;
+  attributes: {
+    Name: string;
+    price: number;
+    stock: number;
+    createdAt: string;
+    updatedAt: string;
+    barcode: null | string;
+    image: {
+      data: {
+        id: number;
+        attributes: Upload;
+      };
+    };
+  };
+};
+
+/** Type représentant un historique dans le backend */
+export type History = {
+  id: number;
+  attributes: {
+    amount: number;
+    price: number;
+    createdAt: string;
+    updatedAt: string;
+    product: {
+      data: Product;
+    };
+  };
+};
