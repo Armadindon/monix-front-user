85dacaf47e48f22c682d7a01ddd7d52cfe563e6b
Armadindon <baptisteperrin77@gmail.com>
2022-09-21 13:40:53 +0300

    Retour menu clic logo Appbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,13 +1,42 @@
+import { IconButton } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useAppDispatch } from "../hook";
+import { changePage } from "../Model/ApplicationSlice";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
   return (
     <>
       <AppBar>
         <Toolbar>
-          <Typography variant="h6" component="div">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="a"
+            href="#"
+            onClick={() => {
+              dispatch(changePage("mainMenu"));
+            }}
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
             Monix 2.0
           </Typography>
         </Toolbar>
