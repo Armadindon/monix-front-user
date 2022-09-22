88ea898126df07e4485c44640b6def71f2e4a8c7
Armadindon <baptisteperrin77@gmail.com>
2022-09-22 09:03:38 +0300

    Added productFilter

--- src/Pages/ProductSelector.tsx
+++ src/Pages/ProductSelector.tsx
@@ -13,6 +13,8 @@ import sendApiRequest from "../Model/WebApi";
 const ProductSelector = () => {
   const dispatch = useAppDispatch();
   const products = useSelector(getProducts);
+  const [displayedProducts, setDisplayedProducts] = useState(products);
+  const [productFilter, setProductFilter] = useState("");
 
   //On crée un state "dictionnaire" ou pour chaque id de produit on met le montant dans l'input
   const [amount, setAmount] = useState<{ [productId in number]: number }>({});
@@ -35,6 +37,13 @@ const ProductSelector = () => {
     // On ignore, car on veut seulement remplir amounts dans le callback
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [products]);
+
+  //Update Product with filter
+  useEffect(() => {
+    setDisplayedProducts(
+      products.filter((prod) => prod.attributes.name.includes(productFilter))
+    );
+  }, [products, productFilter]);
 
   const buyproduct = (product: Product, amount: number) => {
     sendApiRequest({
@@ -67,47 +76,66 @@ const ProductSelector = () => {
         overflowY: "scroll",
       }}
     >
-      {products.map((product) => (
+      <TextField
+        variant="outlined"
+        label="Votre recherche"
+        sx={{ marginTop: "16px", width: "80vw" }}
+        value={productFilter}
+        onChange={(evt) => setProductFilter(evt.currentTarget.value)}
+      />
+      {displayedProducts.map((product) => (
         <Paper
           key={product.id}
           elevation={5}
           sx={{
-            width: "80%",
+            width: "80vw",
+            height: "50vw",
             display: "flex",
             alignContent: "center",
             flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "space-between",
             margin: "10px",
           }}
         >
           {/** Note a soi même : je deteste le mec qui a rendu les api ultra compliqué chez strapi :c */}
           <img
             src={`${process.env.REACT_APP_BACKEND_URL}${product.attributes.image.data.attributes.url}`}
-            style={{ width: "100%", height: "auto" }}
+            style={{
+              maxHeight: "100%",
+              maxWidth: "50vw",
+              width: "auto",
+              height: "auto",
+            }}
             alt={product.attributes.name}
           />
-          <Typography variant="h5">{product.attributes.name}</Typography>
-          <Typography
-            variant="h6"
-            sx={{ display: "flex", alignItems: "center" }}
-          >
-            {product.attributes.price}
-            <MonixCoin style={{ marginLeft: ".25em" }} />
-          </Typography>
-          <Box sx={{ display: "flex" }}>
-            <TextField
-              label="Nombre"
-              sx={{ m: 1, width: "25ch" }}
-              InputProps={{ type: "number", inputProps: { min: 1 } }}
-              value={amount[product.id]}
-              onChange={(evt) => {
-                const newAmount = { ...amount };
-                newAmount[product.id] = Number(evt.currentTarget.value);
-                setAmount(newAmount);
-              }}
-            />
-            <Button onClick={() => buyproduct(product, amount[product.id])}>
-              Acheter !
-            </Button>
+          <Box sx={{ alignSelf: "flex-start", margin: "10px" }}>
+            <Typography variant="h5">{product.attributes.name}</Typography>
+            <Typography
+              variant="h6"
+              sx={{ display: "flex", alignItems: "center" }}
+            >
+              {product.attributes.price}
+              <MonixCoin style={{ marginLeft: ".25em" }} />
+            </Typography>
+            <Box sx={{ display: "flex" }}>
+              <TextField
+                label="Nombre"
+                sx={{ m: 1, width: "25ch" }}
+                InputProps={{ type: "number", inputProps: { min: 1 } }}
+                value={
+                  amount[product.id] !== undefined ? amount[product.id] : 1
+                }
+                onChange={(evt) => {
+                  const newAmount = { ...amount };
+                  newAmount[product.id] = Number(evt.currentTarget.value);
+                  setAmount(newAmount);
+                }}
+              />
+              <Button onClick={() => buyproduct(product, amount[product.id])}>
+                Acheter !
+              </Button>
+            </Box>
           </Box>
         </Paper>
       ))}
