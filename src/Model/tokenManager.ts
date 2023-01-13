fb07f2fc8a1bf2916e6ee9505d6ad5ffcc15daee
Armadindon <baptisteperrin77@gmail.com>
2023-01-13 12:01:48 +0200

    Check expiration date token

--- src/Model/tokenManager.ts
+++ src/Model/tokenManager.ts
@@ -2,8 +2,20 @@ export const setToken = (token: string) => {
   window.localStorage.setItem("token", token);
 };
 
-export const getTokenFromLocalStorage = () =>
-  window.localStorage.getItem("token") || undefined;
+export const getTokenFromLocalStorage = () => {
+  const token = window.localStorage.getItem("token") || undefined;
+  if (token) {
+    const expiration = JSON.parse(atob(token.split(".")[1])).exp;
+    const now = new Date();
+    if (!expiration || now.getTime() > expiration * 1000) {
+      console.log("Le token a expiré ! Merci de vous reconnecter");
+      clearTokenFromLocalStorage();
+      return undefined;
+    }
+    return token;
+  }
+  return undefined;
+};
 
 export const clearTokenFromLocalStorage = () =>
   window.localStorage.removeItem("token");
